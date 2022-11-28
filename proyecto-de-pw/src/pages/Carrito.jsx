ada25707d099118fecc4f4f84a51358c6a383caf
HVR22 <hectorvelardeh22@hotmail.com>
2022-11-28 11:47:36 -0500

    carrito

--- proyecto-de-pw/src/pages/Carrito.jsx
+++ proyecto-de-pw/src/pages/Carrito.jsx
@@ -69,7 +69,7 @@ const Carrito =()=>{
             <div className="col" id="mobile-cart">
                 <div style={{width:"290%",marginTop: "160px"}}>
                 <div style={{marginRight:"-350px",marginLeft:"auto",width:"30%",backgroundColor:"rgba(0, 0, 0, 0.5)",textAlign:"center",borderTopRightRadius:"10px",borderTopLeftRadius:"10px",padding:"10px"}} id="mobile-cart-pad"><b>${getPrice()}</b></div>
-                <div id="list-checkout" style={{width:"90%",marginLeft:"700px",marginRight:"50px",backgroundColor:"rgba(0, 0, 0, 0.5)",padding: "10px",borderRadius:"10px"}} >
+                <div id="list-checkout" style={{width:"90%",marginLeft:"500px",marginRight:"50px",backgroundColor:"rgba(0, 0, 0, 0.5)",padding: "10px",borderRadius:"10px"}} >
                 {showPossibleCheckout(click)}
                 </div>
                 
