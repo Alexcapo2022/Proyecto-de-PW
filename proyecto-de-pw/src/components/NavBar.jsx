101be35fc3d723cce74a1438d2b45d63c1009bad
HVR22 <hectorvelardeh22@hotmail.com>
2022-11-28 03:30:44 -0500

    navbar

--- proyecto-de-pw/src/components/NavBar.jsx
+++ proyecto-de-pw/src/components/NavBar.jsx
@@ -31,45 +31,15 @@ function NavBar() {
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <li><form className="d-flex" role="search">
-    <input type="search" id="inputss-search" placeholder="Buscar aqui" />
-    <div className="contentss-search">
-      <div className="contentss-table">
-        <table id="tabless">
-          <thead>
-            <tr>
-              <td />
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><a href="/specs products/asusrtx3070.html">NVIDIA RTX 3070
-                  <img src="/specs products/media/3070 1.jpg" />
-                </a></td>    
-            </tr>
-            <tr>
-              <td><a href="/specs products/rtx 2070 super.html">NVIDIA RTX 2070 
-                  <img src="/specs products/media/GEFORCE RTX 2070 SUPER.png" /></a></td>
-            </tr>
-            <tr>
-              <td><a href="/specs products/windows 11 home.html">WINDOWS 11 HOME
-                  <img src="/specs products/media/windows-11-home.jpg" /></a></td>
-            </tr>
-            <tr>
-              <td><a href="/specs products/windows 10 pro.html">WINDOWS 10PRO
-                  <img src="/specs products/media/W10-HOME.jpeg" /></a></td>
-            </tr>
-            <tr>
-              <td><a href="/specs products/i3 10gen.html">INTEL CORE I3
-                  <img src="/specs products/media/I3 2.jpg" /></a></td>
-            </tr>
-           
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <button className="btn btn-info" id="index" type="submit">Search</button>
-  </form></li>
+          <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button id="icons" href="Buscar"><FontAwesomeIcon icon={faMagnifyingGlass} /></Button>
+                </Form>
 
           <Nav>
           <Nav.Link href="/User" id="icons"><FontAwesomeIcon icon={faUser} /></Nav.Link>
